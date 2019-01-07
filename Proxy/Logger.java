182fd752bc1cb5c5b69915e18689962f7ccc0efc
Sotaur <christianmarklambert@gmail.com>
2019-01-07 11:52:49 -0700

    Stubbed logger implementing ILogger

---
+++ Proxy/Logger.java
@@ -0,0 +1,40 @@
+package Proxy;
+
+public class Logger implements ILogger {
+
+    @Override
+    public void logAdminMessage(String message) throws Exception {
+
+    }
+
+    @Override
+    public void clearAdminLog() throws Exception {
+
+    }
+
+    @Override
+    public void logRegularMessage(String message) throws Exception {
+
+    }
+
+    @Override
+    public void clearRegularLog() throws Exception {
+
+    }
+
+    @Override
+    public void logGuestMessage(String message) throws Exception {
+
+    }
+
+    @Override
+    public void clearGuestLog() throws Exception {
+
+    }
+
+
+    @Override
+    public void clearAllLogs() throws Exception {
+
+    }
+}
